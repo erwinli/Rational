4ebd0e2c0676b2c85d1ba138804fd15dff731b49
Erwin Li <Erwin_li24@hotmail.com>
2014-10-24 10:13:35 -0700

    Initial Commit

---
+++ Rational/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Rational
+//
+//  Created by Tritonia on 2014-10-24.
+//  Copyright (c) 2014 Tritonia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
